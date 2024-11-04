33583deae560f4a8a178ab607e901039b50fe990
ikuyaa <Ikuyadev@gmail.com>
2024-11-04 07:14:01 -0600

    Cleaned skip scripts

--- src/commands/Music/skip.ts
+++ src/commands/Music/skip.ts
@@ -1,7 +1,7 @@
 import { CommandOptions, SlashCommandProps} from "commandkit";
 import { SlashCommandBuilder, EmbedBuilder, Colors, CommandInteraction, Message } from 'discord.js';
 import {KazagumoPlayer, KazagumoTrack} from "kazagumo";
-import {EmbedGenerator, MessageHelper, Time} from "../../helpers/Helpers";
+import {EmbedGenerator, MessageHelper, MusicHelper, Time} from "../../helpers/Helpers";
 
 
 export const data = new SlashCommandBuilder()
@@ -10,32 +10,9 @@ export const data = new SlashCommandBuilder()
 
 export async function run({interaction, client, handler}: SlashCommandProps) {
     let player: KazagumoPlayer | undefined = client.musicManager.players.get(interaction.guildId!);
-
-    if(!player) {
-        const embed = EmbedGenerator.Error('No player found. Are you playing any music?');
-        await interaction.reply({ embeds: [embed] });
-        MessageHelper.DeleteTimed(interaction, 10);
-        return;
-    }
-
-    if(!player.queue.current) {
-        const embed = EmbedGenerator.Error('No song is currently playing.');
-        await interaction.reply({ embeds: [embed] });
-        MessageHelper.DeleteTimed(interaction, 10);
-        return;
-    }
-    player.skip();
-    if(player.queue.length === 0) {
-        player.destroy();
-        const embed = EmbedGenerator.NoMoreTracks();
-        await interaction.reply({ embeds: [embed] });
-        MessageHelper.DeleteTimed(interaction, Time.secs(10));
-        return;
-    } else {
-        const embed = EmbedGenerator.PlayEmbed(player.queue[0]);
-        await interaction.reply({ embeds: [embed] });
-        return;
-    }
+    
+    await MusicHelper.skipSong(player, interaction);
+    return;
 }
 
 export const options: CommandOptions = {
--- src/helpers/Music/MusicHelper.ts
+++ src/helpers/Music/MusicHelper.ts
@@ -3,6 +3,7 @@ import {Kazagumo, KazagumoPlayer, KazagumoSearchResult} from "kazagumo";
 import client from "../../index";
 import {LogHelper} from "../Helpers";
 import {PlayOptions} from "shoukaku";
+import {EmbedGenerator, MessageHelper, Time} from "../Helpers";
 
 export default class MusicHelper {
     public static async playSong(interaction: Interaction, song: string, player: KazagumoPlayer): Promise<KazagumoSearchResult> {
@@ -37,5 +38,37 @@ export default class MusicHelper {
             throw new Error(err);
         }
         
+    }
+
+    public static async skipSong(player: KazagumoPlayer | undefined, interaction: Interaction) {
+    if(!interaction.isRepliable())
+        return;
+
+    if(!player) {
+        const embed = EmbedGenerator.Error('No player found. Are you playing any music?');
+        await interaction.reply({ embeds: [embed] });
+        MessageHelper.DeleteTimed(interaction, 10);
+        return;
+    }
+
+    if(!player.queue.current) {
+        const embed = EmbedGenerator.Error('No song is currently playing.');
+        await interaction.reply({ embeds: [embed] });
+        MessageHelper.DeleteTimed(interaction, 10);
+        return;
+    }
+
+    player.skip();
+    if(player.queue.length === 0) {
+        player.destroy();
+        const embed = EmbedGenerator.NoMoreTracks();
+        await interaction.reply({ embeds: [embed] });
+        MessageHelper.DeleteTimed(interaction, Time.secs(10));
+        return;
+    } else {
+        const embed = EmbedGenerator.PlayEmbed(player.queue[0]);
+        await interaction.reply({ embeds: [embed] });
+        return;
+    }
     }
 }
